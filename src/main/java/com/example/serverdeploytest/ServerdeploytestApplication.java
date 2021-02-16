18c75d98df4b450fe6ce4194900f57e7b1d88f6a
limjoonchul <dlawnscjf511@naver.com>
2021-02-16 14:27:21 +0900

    Initalize commit

---
+++ src/main/java/com/example/serverdeploytest/ServerdeploytestApplication.java
@@ -0,0 +1,13 @@
+package com.example.serverdeploytest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServerdeploytestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ServerdeploytestApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/serverdeploytest/ServerdeploytestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.serverdeploytest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServerdeploytestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
